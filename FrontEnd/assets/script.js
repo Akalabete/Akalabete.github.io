b45958a848422b0111fc6bace951cb38b89652d7
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-05-10 12:05:08 +0200

    dynamisation de la page op, créa de la page de co

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,47 +1,80 @@
-showAll();
-// permet l'affichage de tous les projets
-function showAll(){
-    fetch("http://localhost:5678/api/works") // recup depuis l'API
-        .then(response => response.json()) // transforme la reponse en json
-        .then(projects =>{ //crée mon tableau de projets
-            console.log(projects); //controle du tableau
-            for (let i = 0 ; i < projects.length; i++) {
-                // crea de la balise gallery
-                const galeryElement = document.querySelector(".gallery");
-                // crea de la balise figure
-                const project = document.createElement("figure");
-                // ajout d'une classe spécifique par catégorie
-                project.className= category(projects[i].category.id);
-                // crea de la balise img
-                const projectImg = document.createElement("img");
-                projectImg.src = projects[i].imageUrl;
-                projectImg.alt = projects[i].title;
-                // crea de la balise figcatption
-                const projectDescription = document.createElement("figcaption");
-                projectDescription.innerText= projects[i].title;
-                // attachement a la div gallery
-                galeryElement.appendChild(project);
-                // attachement au projet
-                project.appendChild(projectImg);
-                project.appendChild(projectDescription);
-        }
-    })
-    //ajout de la class btn active au filtre par défaut
+let projects;
+
+function showAll() {
+  fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+      projects = data;
+      // Afficher les données pour la première fois (par défaut)
+      showProjects(projects);
+    });
+
+  //  bouton "Tout"
+  const bouttonShowAll = document.querySelector(".all");
+  bouttonShowAll.addEventListener("click", function () {
+    showProjects(projects);
+    let currentActiveButton =  document.querySelector(".all");
     inactiveButton();
-    let currentActiveButton = document.querySelector(".all");
     activeButton(currentActiveButton);
+  });
+
+  //  bouton "Objets"
+  const bouttonShowObjects = document.querySelector(".objects");
+  bouttonShowObjects.addEventListener("click", function () {
+    const obj = projects.filter(project => project.category.id === 1);
+    showProjects(obj);
+    let currentActiveButton =  document.querySelector(".objects");
+    inactiveButton();
+    activeButton(currentActiveButton);
+  });
+
+  //  bouton "Appartements"
+  const bouttonShowAppartments = document.querySelector(".appartments");
+  bouttonShowAppartments.addEventListener("click", function () {
+    const apparts = projects.filter(project => project.category.id === 2);
+    showProjects(apparts);
+    let currentActiveButton =  document.querySelector(".appartments");
+    inactiveButton();
+    activeButton(currentActiveButton);
+  });
+
+  // bouton "Hotels & Restaurants"
+  const bouttonShowHotelsRestaurants = document.querySelector(".hotelsRestaurants");
+  bouttonShowHotelsRestaurants.addEventListener("click", function () {
+    const hotelsRestaurants = projects.filter(project => project.category.id === 3);
+    showProjects(hotelsRestaurants);
+    let currentActiveButton =  document.querySelector(".hotelsRestaurants");
+    inactiveButton();
+    activeButton(currentActiveButton);
+  });
 }
-// function qui va renommer la catégorie
-function category(valeur) {
-    if(valeur === 1) {
-        return "obj";
-    } else if(valeur === 2){
-        return "apparts";
-    }else if(valeur === 3){
-        return"hotels_restaurants";
-    }
+
+function showProjects(projects) {
+  const galeryElement = document.querySelector(".gallery");
+  galeryElement.innerHTML = "";
+  for (let i = 0; i < projects.length; i++) {
+    // créa des éléments figures
+    const project = document.createElement("figure");
+    // créa et mise en forme des image avec le alt
+    const projectImg = document.createElement("img");
+    projectImg.src = projects[i].imageUrl;
+    projectImg.alt = projects[i].title;
+    // creé et mise en forme de la description
+    const projectDescription = document.createElement("figcaption");
+    projectDescription.innerText = projects[i].title;
+    // rattachement au parents
+    project.appendChild(projectImg);
+    project.appendChild(projectDescription);
+    galeryElement.appendChild(project);
+  }
+  //ajout de la class btn active au filtre par défaut
+  inactiveButton();
+  let currentActiveButton = document.querySelector(".all");
+  activeButton(currentActiveButton);
 }
-// ajout de la class btn-active et inactive aux autres
+
+
+// fonction qui vont attribuer  btn-active et inactive bouttons
 function inactiveButton(){
     const buttonCount = document.querySelectorAll(".show");
     buttonCount.forEach(function(item){
@@ -54,47 +87,4 @@ function activeButton(currentActiveButton){
     currentActiveButton.classList.add("btn-active");
 }
 
-
-// création des boutons
-
-
-const bouttonShowAll = document.querySelector(".all");
-const bouttonShowObjects = document.querySelector(".objects");
-const bouttonShowAppartments = document.querySelector(".appartments");
-const bouttonShowHotelsRestaurants = document.querySelector(".hotelsRestaurants");
-
-// ajouts des events listeners et des fonctions associées au click
-const apparts = document.getElementsByClassName("apparts");
-const obj = document.getElementsByClassName("obj");
-const hotel = document.getElementsByClassName("hotels_restaurants");
-
-bouttonShowAll.addEventListener('click', function(){
-    let currentActiveButton =  document.querySelector(".all");
-    inactiveButton();
-    activeButton(currentActiveButton);
-    printWhat(all)
-})
-bouttonShowObjects.addEventListener('click', function(){
-    let currentActiveButton =  document.querySelector(".objects");
-    inactiveButton();
-    activeButton(currentActiveButton);
-    printWhat(obj);
-})
-bouttonShowAppartments.addEventListener('click', function(){
-    let currentActiveButton =  document.querySelector(".appartments");
-    inactiveButton();
-    activeButton(currentActiveButton);
-    printWhat(apparts);
-})
-bouttonShowHotelsRestaurants.addEventListener('click', function(){
-    let currentActiveButton =  document.querySelector(".hotelsRestaurants");
-    inactiveButton();
-    activeButton(currentActiveButton);
-    printWhat(hotel)
-})
-
-
-function printWhat(constType){
-    document.querySelector(".gallery").innerHtml = '';
-    show(constType);
-}
+showAll();
