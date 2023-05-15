80112450f2e21d43d2ee9e40c73e033f171aa6dc
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-05-15 17:00:55 +0200

    galerie modale opé, btn sur la seconde modale KO

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,13 +1,13 @@
 // mes variables
 let projects;
 let isModalOpen = false;
+
 // Récupération du token d'authentification
 const token = sessionStorage.getItem("token");
 const modalContainer = document.querySelector(".modal-container");
 const closeCross = document.querySelector(".close-modal-cross");
 
 // creation de la modale au chargement
-
 function createModal(){
   const target = document.querySelector("#modal");
   target.setAttribute('style', 'display: none');
@@ -18,7 +18,6 @@ function createModal(){
 createModal();
 
 // ajout de Sophie a la place du bouton login et verification du token d'auth
-
 if (token) {
   const logged = document.querySelector(".connected");
   logged.innerHTML = 'logout';
@@ -29,24 +28,26 @@ if (token) {
 } else { 
   showAll();
 }
+
 // definition de la fonction logout
 function reinit(event){
   event.preventDefault();
   sessionStorage.removeItem("token");
   location.reload();
-  alert("A bientôt!")
-  console.log(token)
-
+  alert("A bientôt!");
 }
+
 //ajout des liens modifier
 const editLinks = document.querySelectorAll(".js-modal");
 editLinks.forEach(link => {
   link.addEventListener("click", openModal);
 });
+
 // ouverture de la modale au click
 function openModal(event) {
   event.stopPropagation(); // Empêche la fermeture immédiate de la modale
   //console.log("clicked");
+
   // changement des attributs de la modale
   const target = document.querySelector("#modal");
   target.setAttribute('style', 'display: flex');
@@ -55,8 +56,10 @@ function openModal(event) {
   //console.log(target);
   isModalOpen = true;
   //console.log(isModalOpen);
+
   //creation du contenu dynamique de la modale
   dynamicModalContent(projects);
+
   // Ajout de la possibilité de quitter la modale
   // en cliquant hors champs, désynchronisé
   setTimeout(function() {
@@ -64,11 +67,13 @@ function openModal(event) {
     addEvent();
   }, 100);
 }
+
 //  ajout de l'eventlistener
 function addEvent() {  
   window.addEventListener("click", outsideClick);
   closeCross.addEventListener("click", outsideClick);
 }
+
 // fonction qui verifie que l'on clique bien en dehors de la
 // modale ou sur la croix
 function outsideClick(event) {
@@ -76,6 +81,7 @@ function outsideClick(event) {
     closeModal();
   }
 }
+
 // fonction qui ferme la modale
 function closeModal() {
   //console.log(isModalOpen);
@@ -89,9 +95,8 @@ function closeModal() {
     return;
   }
 }
+
 // affichage du contenu dynamique
-
-
 function showAll() {
   fetch("http://localhost:5678/api/works")
     .then(response => response.json())
@@ -141,6 +146,7 @@ function showAll() {
   });
 }
 
+// contructeur de la mini gallerie
 function showProjects(projects) {
   const galeryElement = document.querySelector(".gallery");
   galeryElement.innerHTML = "";
@@ -159,12 +165,12 @@ function showProjects(projects) {
     project.appendChild(projectDescription);
     galeryElement.appendChild(project);
   }
+
   //ajout de la class btn active au filtre par défaut
   inactiveButton();
   let currentActiveButton = document.querySelector(".all");
   activeButton(currentActiveButton);
 }
-
 
 // fonction qui vont attribuer  btn-active et inactive bouttons
 function inactiveButton(){
@@ -179,24 +185,48 @@ function activeButton(currentActiveButton){
     currentActiveButton.classList.add("btn-active");
 }
 
+// constructeur de la mini gallerie
 function dynamicModalContent(projects){
+  // bouton ajout d'une photo
+  const modalTwo = document.querySelector(".two");
+  modalTwo.setAttribute("style", "display: none");
+  const addPhoto = document.querySelector(".photo-add");
+  addPhoto.addEventListener('click', addNewPic);
   const galeryElement = document.querySelector("#modal-gallery");
   galeryElement.innerHTML = "";
   for (let i = 0; i < projects.length; i++) {
-    // créa des éléments figures
     const project = document.createElement("figure");
-    // créa et mise en forme des image avec le alt
     const projectImg = document.createElement("img");
     projectImg.src = projects[i].imageUrl;
     projectImg.alt = projects[i].title;
-    // creé et mise en forme de la description
+
+    // ajout du lien d'édition
     const projectEdition = document.createElement("a");
     projectEdition.innerText = "éditer";
-    // rattachement au parents
     project.appendChild(projectImg);
     project.appendChild(projectEdition);
     galeryElement.appendChild(project);
   }
 }
 
+//permutation entre les pages de la modale
+function addNewPic(){
+  const modalOne = document.querySelector(".one");
+  modalOne.setAttribute("style", "display: none");
+  const modalTwo = document.querySelector(".two");
+  modalTwo.setAttribute("style", "display: flex")
+}
+// creation du formulaire d'ajout de photo
 
+
+// annulation de l'ajout de photo avec le bouton <-
+//const backBtn = document.querySelector(".cancel-add");
+//backBtn.addEventListener("click", openModal);
+
+function backToModalOne(){
+  
+  const modalTwo = document.querySelector(".two");
+  modalTwo.setAttribute("style", "display: none");
+  const modalOne = document.querySelector(".one");
+  modalOne.setAttribute("style", "display: flex")
+}
